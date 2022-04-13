9207c542cc268c73745ba7d6396eec8dbff198c5
Timur <2001rich04@gmail.com>
2022-04-14 02:59:52 +0300

    Create User class

---
+++ TestBot.BLL/User.cs
@@ -0,0 +1,13 @@
+﻿namespace TestBot.BLL
+{
+    public class User
+    {
+        public string Name { get; private set; }
+        public string ChatId { get; private set; }
+
+        public void ChangeName(string newName)
+        {
+            Name = newName;
+        }
+    }
+}
