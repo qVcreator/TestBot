91e97f1530d8e378c353ceceb18cc3bf3b1d0351
Timur <2001rich04@gmail.com>
2022-04-14 03:18:01 +0300

    Create Group class

--- TestBot.BLL/User.cs
+++ TestBot.BLL/User.cs
@@ -3,7 +3,13 @@
     public class User
     {
         public string Name { get; private set; }
-        public string ChatId { get; private set; }
+        public long ChatId { get; private set; }
+
+        public User(string name, long chatId)
+        {
+            Name = name;
+            ChatId = chatId;
+        }
 
         public void ChangeName(string newName)
         {
