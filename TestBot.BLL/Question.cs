aa93a7e0965e1a05aa7c3f4f490915f67f224f60
Alice <alisafoxpirs@gmail.com>
2022-04-17 15:59:48 +0300

    Add Question and TestQuestion

---
+++ TestBot.BLL/Question.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBot.BLL
+{
+    abstract public class Question
+    {
+        public string Text { get; set; }
+
+        public List<Answer> Answers { get; set; }
+
+        public void ChangeText(string newText)
+        {
+            if(newText == null)
+            {
+                throw new Exception("newText must not be null");
+            }
+            Text = newText;
+        }
+    }
+
+    public class Answer
+    {
+        public string Text { get; set; }
+    }
+}
