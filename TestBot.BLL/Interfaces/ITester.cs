04cbb876e04050cbdb5270df4a31be151fa69f87
Emil <intestinal_d@rambler.ru>
2022-04-23 03:16:20 +0300

    Added question type Interfaces

---
+++ TestBot.BLL/Interfaces/ITester.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBot.BLL.Interfaces
+{
+    public interface ITester
+    {
+        public void CheckInput();
+
+        public void CheckAnswer();
+    }
+}
