9bf9effaa3fc3c90545a53977e5b51d4cf6b9f9f
Emil <intestinal_d@rambler.ru>
2022-05-05 01:11:50 +0300

    Initialized QuestionData class
    To get data for ComboBoxTestNameSelect

---
+++ TestBot.BLL/QuestionData.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestBot.BLL
+{
+    public class QuestionData
+    {
+        public string Description { get; set; }
+
+        public string Type { get; set; }
+
+        public QuestionData(string type, string description)
+        {
+
+            Type = type;
+
+            Description = description;
+        }
+    }
+}
